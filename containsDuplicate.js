6d1c5201d3f6f922456ec870cf2d66f4f0a30e33
Alan Ng <alanngyf@gmail.com>
2015-09-28 18:10:14 -0700

    updated leetcode problem # 217,  containsDuplicate.js (hash method)

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -22,3 +22,16 @@ var containsDuplicate = function(nums) { //runtime is too slow
     }
     return false;
 };
+
+// hash method, suggested by sue, faster run time, tradeoff, increae space needed!
+var containsDuplicate = function(nums) { 
+    var hash = {};
+    for(var i=0; i<nums.length; i++) {
+        if(hash.hasOwnProperty(nums[i])){
+            return true;
+        }else{
+            hash[nums[i]] = 1;
+        }
+    }
+    return false;
+};
